f4ec7f865132e086aa9ecd15fbfd1da42ed50345
Quoc-Khuong Tran <quockhuongtran17@gmail.com>
2023-10-31 18:57:29 +0700

    abc

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a27c72562fbe24d15809986bc21b1d55-1929974595.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
